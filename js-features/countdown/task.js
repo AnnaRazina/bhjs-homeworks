2d82e9d8aa452a9747047ff0fbdb87e796fc736a
Anna Razina <vega_b@mail.ru>
2022-07-19 15:32:57 +0400

    Таймер обратного отсчёта

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -2,7 +2,6 @@ const countdown = function () {
     let timer = document.getElementById("timer");
     timer.textContent -= 1; 
 }
-
 let timerId = setInterval(countdown, 1000);
 
 setTimeout(() => {clearInterval(timerId); 
