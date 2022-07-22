d482602569347985973b868c5214e1f4aa011d7a
Anna Razina <vega_b@mail.ru>
2022-07-22 23:44:39 +0400

    Игра-кликер + задание повышенного уровеня сложности

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -3,26 +3,36 @@ let clicker__counter = document.getElementById("clicker__counter");
 let countClicks = 0;
 
 function changeSizes () {   
-  if (countClicks % 2 == 0) {
+  if (countClicks % 2 === 0) {
     cookie.width = 400;
   } else {cookie.width = 200;}   
 }
 
-//второй вариант функции changeSizes
-/*function changeSizes () { 
-    if (cookie.width !== 400) {
-      cookie.width = 400;
-      return changeSizes;
-    } 
-    if (cookie.width = 400) {
-        cookie.width = 200;
-    }   
-  }*/
-
 function counter () {
-    clicker__counter.textContent++
     countClicks += 1; 
+    clicker__counter.textContent = countClicks;
+}
+
+const divClickSpeed = document.createElement("div");
+divClickSpeed.textContent = "Скорость клика: ";
+const clicker__status = document.querySelector(".clicker__status");
+clicker__status.appendChild(divClickSpeed);
+
+divClickSpeed.insertAdjacentHTML("beforeEnd", "<span>0</span>");
+let ClickSpeed = divClickSpeed.querySelector("span");
+
+let PreviousClickTime, CurrentClickTime; 
+function ClickSpeedScore () {
+  if (countClicks === 1) {
+    PreviousClickTime = new Date().getTime();
+  }
+   if (countClicks !== 1) {
+    CurrentClickTime = new Date().getTime();
+    ClickSpeed.textContent = (1000 / (CurrentClickTime - PreviousClickTime)).toFixed(2);
+    PreviousClickTime = CurrentClickTime;
+  }
 }
 
 cookie.addEventListener("click", changeSizes);
 cookie.addEventListener("click", counter);
+cookie.addEventListener("click", ClickSpeedScore);
