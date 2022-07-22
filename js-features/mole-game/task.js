cb83a9defe24075864871bbdfb83a3d0c66307ea
Anna Razina <vega_b@mail.ru>
2022-07-22 23:39:50 +0400

    Игра «Убей кротов»

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,24 @@
+let dead = document.getElementById("dead");
+let lost = document.getElementById("lost");  
+
+getHole = index => document.getElementById(`hole${index}`);
+
+for (let i = 1; i <= 9; i++) {
+    let hole = getHole(i);
+    hole.onclick = function() {
+        if (hole.className.includes("hole_has-mole") === true) {
+            dead.textContent++;
+            if (dead.textContent === "10") {
+                alert ("Вы победили!");
+                dead.textContent = 0;
+                lost.textContent = 0; 
+            }
+        } else {lost.textContent++
+            if (lost.textContent === "5") {
+                alert ("Вы проиграли");
+                dead.textContent = 0;
+                lost.textContent = 0;
+            }
+        }
+    }
+}
