9bf8fb946db7e2133f3517b7b4752f2f0245da44
Anna Razina <vega_b@mail.ru>
2022-07-27 16:44:52 +0400

    доработка 2 - проверка наличия подменю

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,15 @@
 let getMenuLink = Array.from(document.querySelectorAll(".menu__link"));
+
 getMenuLink.forEach(link => link.addEventListener("click", (event) => {
+
     let activeMenuSub = document.querySelector(".menu_active");
+
+    if (link.closest(".menu__item").querySelector(".menu_sub")) {
     if (event.target.closest(".menu__item").querySelector(".menu_sub").classList.contains("menu_active")) {
         event.preventDefault();
         activeMenuSub.classList.remove("menu_active");
         return 
-    }
+    }}
     if (activeMenuSub) {
         activeMenuSub.classList.remove("menu_active")   
     }
