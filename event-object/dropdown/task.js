b6b92e7df15fe8bd1aa26b2f4abacc3d3a13215b
Anna Razina <vega_b@mail.ru>
2022-07-31 00:02:04 +0400

    задание - Выпадающие списки

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,26 +1,26 @@
-const dropdownItem = Array.from(document.querySelectorAll(".dropdown__item"));
-console.log(dropdownItem);
-let dropdownValue = document.querySelectorAll(".dropdown__value");
-console.log(dropdownValue);
-//let dropdownList = document.querySelector(".dropdown__list");
-//console.log(dropdownList);
-let dropdownList = dropdownValue[0].nextElementSibling
-console.log(dropdownList);
+let dropdownValue = Array.from(document.querySelectorAll(".dropdown__value"));
 
+function oupenDropdownList () {
+  dropdownValue.forEach(element => {element.addEventListener("click", (event) => {
+      event.preventDefault();
+      let dropdownList = element.nextElementSibling;
+      if(dropdownList) {dropdownList.classList.toggle("dropdown__list_active")
+      } else {return}
 
-
-if (dropdownList.classList.contains("dropdown__list_active")) {
-   
-    dropdownItem.forEach(element => {element.addEventListener("click", (event) => {
-        let href = event.target.getAttribute('href');
-console.log(element)
-})
+      let dropdownListActive = Array.from(document.querySelectorAll(".dropdown__list_active"));
     
-})
+      if(dropdownListActive.length > 0) {
+         for(let i = 0; i < dropdownListActive.length; i++) {
+          let dropdownItem = Array.from(dropdownListActive[i].children);
+          dropdownItem.forEach(el => {el.addEventListener("click", (event) => {
+            event.preventDefault() 
+            let item = el.textContent;
+            let btnDropdownItem = dropdownListActive[i].previousElementSibling;
+            btnDropdownItem.textContent = item;
+            dropdownListActive[i].classList.remove("dropdown__list_active")
+            })})
+         }    
+      }
+  })})
 }
-
-dropdownValue[0].addEventListener("click", () => {
-    if(dropdownItem.length > 0) {
-     dropdownList.classList.toggle("dropdown__list_active")
-    }
- })
+oupenDropdownList()
