286c864358d9c50c18c9354f2efac3c5264ed4a6
Anna Razina <vega_b@mail.ru>
2022-08-03 13:50:54 +0400

    Выпадающий список с поиском

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,25 +68,16 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
-
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
-      }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    let arrayMatches = [];
+    Array.from(this.input.options).forEach(el => {
+      if (el.textContent.includes(text) && text.trim().length > 0){
+        arrayMatches.push({
+          text: `${el.text}`,
+          value: `${el.value}`
+        })
+      } 
+    })
+    return arrayMatches
   }
 }
 
